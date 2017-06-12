9bfe2bdb874141d2256644e7613d8d223beee009
haofangyuan <13581535474@163.com>
2017-06-12 15:44:27 +0800

    Site updated: 2017-06-12 15:44:27

--- js/gitment.browser.js
+++ js/gitment.browser.js
@@ -2897,6 +2897,7 @@ function renderComments(_ref2, instance) {
     var errorBlock = document.createElement('div');
     errorBlock.className = 'gitment-comments-error';
     alert(user.login + ":" + instance.owner);
+    alert(error + " : " + _constants.NOT_INITIALIZED_ERROR);
     if (error === _constants.NOT_INITIALIZED_ERROR && user.login && user.login.toLowerCase() === instance.owner.toLowerCase()) {
       var initHint = document.createElement('div');
       var initButton = document.createElement('button');
